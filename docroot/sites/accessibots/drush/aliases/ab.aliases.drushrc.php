178939495f0137bcc7034d66a5545c764a92111a
KeyboardCowboy <chris@162unlimited.com>
2015-03-24 23:16:43 -0600

    Add drush directories.

---
+++ docroot/sites/accessibots/drush/aliases/ab.aliases.drushrc.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Aliases for Accessibots
+ */
+
+// Parent aliases
+$aliases['_remote'] = array(
+  'remote-host' => '204.62.14.91',
+  'remote-user' => 'accessibot',
+  'path-aliases' => array(
+    '%dump-dir' => '/var/www/db',
+  ),
+);
+
+// Stage
+$aliases['stage'] = array(
+  'parent' => '@ab._remote',
+  'root' => '/var/www/htdocs/accessibots/stage/docroot',
+  'uri' => 'stage.accessibots.io',
+);
+
+// Prod
+$aliases['prod'] = array(
+  'parent' => '@ab._remote',
+  'root' => '/var/www/htdocs/accessibots/prod/docroot',
+  'uri' => 'accessibots.io',
+);
