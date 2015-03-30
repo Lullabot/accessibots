093c1781560f6a66645de0ebada5b35c0f8f9a1e
KeyboardCowboy <chris@162unlimited.com>
2015-03-30 15:09:34 -0600

    Add composer build.

--- util/jenkins/accessibots_prod_deploy.sh
+++ util/jenkins/accessibots_prod_deploy.sh
@@ -3,5 +3,12 @@ set -ex
 
 REPO="git@github.com:Lullabot/accessibots.git"
 ROOT="/var/www/htdocs/accessibots/prod"
+WORKSPACE="/home/jenkins/jobs/Accessibots Prod Deploy/workspace/"
 
 cd $ROOT
+
+if [ -d "vendor" ]; then
+  php ../composer.phar update
+else
+  php ../composer.phar install
+fi
