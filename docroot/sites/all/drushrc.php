3f8d25823e0e8e583bda5e7d1c518d7e462bd6c8
KeyboardCowboy <chris@162unlimited.com>
2015-04-01 21:39:24 -0600

    Update drush plugins.

--- docroot/sites/all/drushrc.php
+++ docroot/sites/all/drushrc.php
@@ -1,9 +1,7 @@
 <?php
 // Patchfile Settings.
 $options['patch-file'] = '../patches.make';
-
 // Add our commands directory.
 $options['include'][] = __DIR__ . '/drush/commands';
-
 // Add our aliases.
 $options['alias-path'][] = __DIR__ . '/drush/aliases';
