b0fb89da0d376770d613b53919a24abe91d6a03b
KeyboardCowboy <chris@162unlimited.com>
2015-04-02 01:39:29 -0600

    Adjust paths in deploy script.

--- docroot/sites/all/drushrc.php
+++ docroot/sites/all/drushrc.php
@@ -3,7 +3,7 @@
 $options['patch-file'] = '../patches.make';
 
 // Add our commands directory.
-$options['include'][] = __DIR__ . '/drush/commands';
+$options['include'][] = __DIR__ . '/drush/plugins';
 
 // Add our aliases.
 $options['alias-path'][] = __DIR__ . '/drush/aliases';
--- util/jenkins/accessibots_prod_deploy.sh
+++ util/jenkins/accessibots_prod_deploy.sh
@@ -3,9 +3,10 @@ set -ex
 
 REPO="git@github.com:Lullabot/accessibots.git"
 ROOT="/var/www/htdocs/accessibots/prod"
+WEBROOT="/var/www/htdocs/accessibots/prod/docroot"
 WORKSPACE="/home/jenkins/jobs/Accessibots Prod Deploy/workspace/"
 
-cd $ROOT
+cd $WEBROOT
 
 # Run updates, reverts and flushes
 drush reset
