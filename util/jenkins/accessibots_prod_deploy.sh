3544923aef55122b8c8409dcea3e1986287c5135
KeyboardCowboy <chris@162unlimited.com>
2015-04-02 17:34:55 -0600

    Move drush files outside of the repo and build with composer.

--- util/jenkins/accessibots_prod_deploy.sh
+++ util/jenkins/accessibots_prod_deploy.sh
@@ -6,6 +6,15 @@ ROOT="/var/www/htdocs/accessibots/prod"
 WEBROOT="/var/www/htdocs/accessibots/prod/docroot"
 WORKSPACE="/home/jenkins/jobs/Accessibots Prod Deploy/workspace/"
 
+cd $ROOT
+
+# Update composer
+if [ -d vender ]; then
+  php ./composer.phar update
+else
+  php ./composer.phar install
+fi
+
 cd $WEBROOT
 
 # Run updates, reverts and flushes
