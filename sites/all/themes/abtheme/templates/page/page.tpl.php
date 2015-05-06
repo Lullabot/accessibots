8aafe8e847ccb7a4f5704b2fe94c6a0a03d151b0
KeyboardCowboy <chris@162unlimited.com>
2015-05-06 00:52:39 -0600

    [#1] Theme non-homepage.

--- sites/all/themes/abtheme/templates/page/page.tpl.php
+++ sites/all/themes/abtheme/templates/page/page.tpl.php
@@ -4,13 +4,12 @@
   <nav id="main-nav" role="navigation">
     <?php print render($main_menu); ?>
   </nav>
+  <?php print render($title_prefix); ?>
+  <?php if ($title): ?>
+    <h1><?php print $title; ?></h1>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
 </header>
-
-<?php print render($title_prefix); ?>
-<?php if ($title): ?>
-  <h1><?php print $title; ?></h1>
-<?php endif; ?>
-<?php print render($title_suffix); ?>
 
 <?php print render($page['content']); ?>
 
